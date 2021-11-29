2d2d895d4abae0853acee46eeea4bf257377cbeb
rasik <rasikvarshney215@gmail.com>
2021-11-29 19:53:08 +0530

    finished

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,10 @@
 function CreateObject(arr) {
     // Write your code here
+    var obj={}
+    for(var i=0; i<arr.length; i=i+2){
+        obj[arr[i]]=arr[i+1]
+    }
+    return obj
 }
 
 module.exports = CreateObject;
