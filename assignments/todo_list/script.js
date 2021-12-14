111b1bc08c6bf03d56d2bb1fa7cd62c60cf059ee
Radhakant Tiwari <therktiwadi@gmail.com>
2021-12-14 20:59:02 +0530

    todo list api done

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,25 +1,101 @@
-var task_count = 1;
-
-function AddTask() {
-  var task = document.getElementById("taskin");
-  var taskdiv = document.getElementById("todoDiv");
-  var taskp = document.createElement("p");
-  var text = document.createTextNode(task.value);
-  task_count++;
-
-  //setting up each item
-  var idname = task_count;
-  taskp.appendChild(text);
-  taskdiv.appendChild(taskp);
-  taskp.setAttribute("id", "idname");
-  taskp.setAttribute("onClick", "RemoveTask(this.id)");
-
-  //console and count
-  console.log(task.value);
-  console.log(task_count);
+let select = document.getElementById('dropdown');
+let answer = document.getElementById('answer');
+var completed = [];
+var pending = [];
+​
+async function funcName(url){
+    const response = await fetch(url);
+    var data = await response.json();
+    console.log(data);
+    for(let i=0; i<data.length;i++){
+        if(data[i]['completed']===true){
+            completed.push(data[i]);
+        }else{
+            pending.push(data[i]);
+        }
+    }
+​
+​
+    select.addEventListener('change', ()=>{
+        let status = select.value;
+        if(status==='completed'){
+            answer.innerHTML = "";
+​
+            for(let i=0; i<completed.length; i++){
+​
+                let div = document.createElement('div');
+                div.setAttribute('class','rows');
+​
+                let h1 = document.createElement('h4');
+                let h2 = document.createElement('h4');
+                let h3 = document.createElement('h4');
+                let h4 = document.createElement('h4');
+​
+                h1.innerText = completed[i].userId;
+                h2.innerText = completed[i].id;
+                h3.innerText = completed[i].title;
+                h4.innerText = completed[i].completed;
+​
+                div.appendChild(h1);
+                div.appendChild(h2);
+                div.appendChild(h3);
+                div.appendChild(h4);
+​
+                answer.appendChild(div)
+            }
+        }else if(status === 'pending'){
+            answer.innerHTML = "";
+​
+            for(let i=0; i<pending.length; i++){
+​
+                let div = document.createElement('div');
+                div.setAttribute('class','rows');
+​
+                let h1 = document.createElement('h4');
+                let h2 = document.createElement('h4');
+                let h3 = document.createElement('h4');
+                let h4 = document.createElement('h4');
+​
+                h1.innerText = pending[i].userId;
+                h2.innerText = pending[i].id;
+                h3.innerText = pending[i].title;
+                h4.innerText = pending[i].completed;
+​
+                div.appendChild(h1);
+                div.appendChild(h2);
+                div.appendChild(h3);
+                div.appendChild(h4);
+​
+                answer.appendChild(div)
+            }
+        }else{
+            answer.innerHTML = "";
+​
+            for(let i=0; i<data.length; i++){
+​
+                let div = document.createElement('div');
+                div.setAttribute('class','rows');
+​
+                let h1 = document.createElement('h4');
+                let h2 = document.createElement('h4');
+                let h3 = document.createElement('h4');
+                let h4 = document.createElement('h4');
+​
+                h1.innerText = data[i].userId;
+                h2.innerText = data[i].id;
+                h3.innerText = data[i].title;
+                h4.innerText = data[i].completed;
+​
+                div.appendChild(h1);
+                div.appendChild(h2);
+                div.appendChild(h3);
+                div.appendChild(h4);
+​
+                answer.appendChild(div)
+            }
+        }
+    })
 }
-
-function RemoveTask(ele) {
-  var toremove = document.getElementById(ele);
-  toremove.remove();
-}
+​
+var url = 'https://jsonplaceholder.typicode.com/todos';
+var total = funcName(url);
